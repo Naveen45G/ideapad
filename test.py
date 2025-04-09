63dc5faf571b9cd1eea39f96b6071d72b045ca7c
Bandi <naveenbandi782@gmail.com>
2025-04-09 22:50:21 +0530

    client2boto4 test.py

--- test.py
+++ test.py
@@ -1,6 +1,6 @@
 aws console
 #def lambda_handler(event, context):
-  client = boto3.client('ec2')
+  client = boto4.client('ec2')
   response = client.run_instances(
     ImageId='ami-001843b876406202a',
     InstanceType='t2.micro',
